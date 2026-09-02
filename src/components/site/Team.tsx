b92acd69bc07b0740ea4cb240653a4a733a61fee
Vaibhav Nehra <vaibhavnehra2005@gmail.com>
2026-09-02 18:59:29 +0530

    Add photo for Prof. R. K. Mishra

--- src/components/site/Team.tsx
+++ src/components/site/Team.tsx
@@ -6,6 +6,7 @@ import shailendraKumarSingh from "@/assets/shailendra-kumar-singh.png";
 import pkMishra from "@/assets/pk-mishra.png";
 import dkSingh from "@/assets/dk-singh.png";
 import rsSingh from "@/assets/rs-singh.png";
+import rkMishra from "@/assets/rk-mishra.png";
 
 const COMMITTEE = [
   {
@@ -66,7 +67,7 @@ const ADVISORY = [
   { name: "Prof. P. K. Mishra", role: "Former Vice-Chancellor, JUT, Ranchi & AKTU, Lucknow", dept: "IIT (BHU), Varanasi", image: pkMishra },
   { name: "Prof. D. K. Singh", role: "Vice-Chancellor, JUT, Ranchi", dept: "Former Director, BIT Sindri", image: dkSingh },
   { name: "Prof. Ram Sharan Singh", role: "Head, Department of Chemical Engineering & Technology", dept: "IIT (BHU), Varanasi", image: rsSingh },
-  { name: "Prof. R. K. Mishra", role: "Department of Electrical Engineering", dept: "IIT (BHU), Varanasi" },
+  { name: "Prof. R. K. Mishra", role: "Department of Electrical Engineering", dept: "IIT (BHU), Varanasi", image: rkMishra },
   { name: "Prof. Santosh Ansumali", role: "Jawaharlal Nehru Centre for Advanced Scientific Research", dept: "(JNCASR), Bengaluru" },
   { name: "Dr. Preetam Singh", role: "Department of Ceramic Engineering", dept: "IIT (BHU), Varanasi" },
   { name: "Dr. Harish Kumar", role: "Scientist G", dept: "DMSRDE, Kanpur" },
